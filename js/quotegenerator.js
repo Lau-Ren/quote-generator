cc17b93969b4b11dd204496cbbab30e8147d9ff8
Lauren Colls <lauren.colls@hotmail.com>
2016-04-13 20:59:23 +1200

    add css formatting

--- js/quotegenerator.js
+++ js/quotegenerator.js
@@ -1,20 +1,21 @@
 
 
-var HTMLquote = '<h1 id="quoteText">%data%</h1><hr>';
+var HTMLquote = '<h1 id="quoteText"><i class="fa fa-quote-left" aria-hidden="true"></i>%data% <i class="fa fa-quote-right" aria-hidden="true"></i></h1><hr>';
 var HTMLauthor= '<span>%data%</span>';
+var HTMLpageColour= '<body style="background-color:%data%">'
 
 var quotes = {
 	"quotes" : [{
 		"words" : "Java is to JavaScript what Car is to Carpet.",
 		"author" : "Chris Heilmann",
-		"colour" : " "
+		"colour" : "colour1" 
 
 
 	},
 	{
-		"words" : "If debugging is the process of removing software bugs, then programming must be the process of putting them in. ".
+		"words" : "If debugging is the process of removing software bugs, then programming must be the process of putting them in. ",
 		"author" : "Edsger Dijkstra",
-		"colour" : " "
+		"colour" : "colour2"
 
 
 
@@ -22,7 +23,7 @@ var quotes = {
 	{
 		"words" : "It's not at all important to get it right the first time. It's vitally important to get it right the last time.",
 		"author" : "Andrew Hunt and David Thomas", 
-		"colour" : " "
+		"colour" : "colour3"
 
 
 
@@ -30,7 +31,7 @@ var quotes = {
 	{
 		"words" : "First, solve the problem. Then, write the code.",
 		"author" : "John Johnson",
-		"colour" : " "
+		"colour" : "colour4"
 
 
 
@@ -38,7 +39,7 @@ var quotes = {
 	{
 		"words" : "Should array indices start at 0 or 1? My compromise of 0.5 was rejected without, I thought, proper consideration.",
 		"author" : "Stan Kelly-Bootle", 
-		"colour" : " "
+		"colour" : "colour5"
 
 
 
@@ -46,7 +47,7 @@ var quotes = {
 	{
 		"words" : "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live. ",
 		"author" : "Rick Osborne", 
-		"colour" : " "
+		"colour" : "colour5" 
 
 
 
@@ -58,14 +59,49 @@ var quotes = {
 
 var formattedQuote = HTMLquote.replace("%data%", quotes.quotes.words);
 
-var formattedAuthor = HTMLAuthor.replace("%data%", quotes.quotes.author);
+var formattedAuthor = HTMLauthor.replace("%data%", quotes.quotes.author);
+var formattedBGColour =HTMLpageColour.replace("%data%", quotes.quotes.colour);
+
+$("#quote").prepend(formattedQuote);
+$("#author").prepend(formattedAuthor);
+$("body").prepend(formattedBGColour);
+
+
+	var randomNum
 
 
 
 $(document).ready(function() {
+	getRandomNumber(0, quotes.quotes.length - 1)
+	changeQuote ()
+
   $('button').click(function() {
-  	var randomNum = 
-    var newQuote = quotes.quotes[randomNum] || function(){};
-    $('#quote').html(iName);  
+		getRandomNumber(0, quotes.quotes.length -1)
+    changeQuote ()
+ 
   });
 });
+
+var newBGColour 
+
+function getRandomNumber(min, max) {
+	$('body').removeClass(newBGColour);
+	$('button').removeClass(newBGColour);
+	randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+	console.log(randomNum)
+  return randomNum
+}
+
+function changeQuote (){
+	newBGColour = quotes.quotes[randomNum].colour || function(){};
+	$('body').addClass(newBGColour);
+	$('button').addClass(newBGColour);
+	var newQuote = quotes.quotes[randomNum].words || function(){};
+	$('#quote').html(newQuote); 
+
+	 var newAuthor = quotes.quotes[randomNum].author || function(){};
+	$('#author').html(newAuthor); 
+
+	 
+
+}
